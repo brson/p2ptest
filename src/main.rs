5ed61f7b64e3f3ce4704a4c37432a75658b1ea0f
Brian Anderson <andersrb@gmail.com>
2022-12-29 23:25:50 -0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 use anyhow::Result;
-use libp2p::{identity, PeerId};
-use libp2p::ping;
+use libp2p::{identity, ping, PeerId, Multiaddr};
+use libp2p::swarm::{Swarm, SwarmEvent, NetworkBehaviour};
+use libp2p::swarm::keep_alive;
+use libp2p::futures::StreamExt;
+use void::Void;
+
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "MyBehaviourEvent")]
+struct MyBehaviour {
+    ping: ping::Behaviour,
+    keep_alive: keep_alive::Behaviour,
+}
+
+#[derive(Debug)]
+enum MyBehaviourEvent {
+    Ping(ping::Event),
+    KeepAlive(Void),
+}
+
+impl From<ping::Event> for MyBehaviourEvent {
+    fn from(event: ping::Event) -> MyBehaviourEvent {
+        MyBehaviourEvent::Ping(event)
+    }
+}
+
+impl From<Void> for MyBehaviourEvent {
+    fn from(event: Void) -> MyBehaviourEvent {
+        MyBehaviourEvent::KeepAlive(event)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -10,7 +38,28 @@ async fn main() -> Result<()> {
 
     let transport = libp2p::tokio_development_transport(local_key)?;
 
-    let behaviour = ping::Behaviour::new(ping::Config::new().with_keep_alive(true));
+    let behaviour = MyBehaviour {
+        ping: ping::Behaviour::new(ping::Config::new()),
+        keep_alive: keep_alive::Behaviour::default(),
+    };
 
-    Ok(())
+    let mut swarm = Swarm::with_tokio_executor(transport, behaviour, local_peer_id);
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {}", addr)
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
+            SwarmEvent::Behaviour(event) => println!("{:?}", event),
+            e => {
+                println!("unhandled event: {:#?}", e);
+            }
+        }
+    }
 }
